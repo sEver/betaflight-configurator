aa13982bd3678cd94beb1f74abd558446a5b04fb
sEver <sEver@users.noreply.github.com>
2025-03-08 01:28:01 +0100

    Rename .firmware_flasher_button to .firmware_flasher_button_box

--- src/js/Analytics.js
+++ src/js/Analytics.js
@@ -51,7 +51,7 @@ function setupAnalytics(result) {
     tracking.sendEvent(tracking.EVENT_CATEGORIES.APPLICATION, "AppStart", { sessionControl: "start" });
 
     $(".connection_button a.connect").removeClass("disabled");
-    $(".firmware_flasher_button a.flash").removeClass("disabled");
+    $(".firmware_flasher_button_box a.flash").removeClass("disabled");
 }
 
 export function checkSetupAnalytics(callback) {
